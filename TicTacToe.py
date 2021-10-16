0ce8e878eed98cd6eecd459020f0543fbe58915a
Your Name <you@example.com>
2021-10-16 11:40:44 +0300

    Добавил метод check_win() который в свою очередь определяет победителя между пользователями.

--- TicTacToe.py
+++ TicTacToe.py
@@ -26,3 +26,11 @@ def take_input(player_token):
         else:
             print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
+
+def check_win(board) -> object:
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
